7ed9296f88c247c79c39841f5aa5c678559f9509
studentSabaG12 <127537149+studentSabaG12@users.noreply.github.com>
2024-04-27 14:12:11 +0400

    added authentication middleware

---
+++ PersonIdentification/MiddleWare/AuthenticationMiddleware.cs
@@ -0,0 +1,25 @@
+﻿namespace PersonIdentification.MiddleWare
+{
+    public class AuthenticationMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public AuthenticationMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            if (!context.User.Identity.IsAuthenticated)
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync("Not Authorized");
+                return;
+            }
+            await _next(context);
+        }
+
+    }
+}
+
+
+
